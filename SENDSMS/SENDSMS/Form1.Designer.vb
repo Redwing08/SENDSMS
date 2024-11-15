e164833bc227eef71918f8927ac8e7bb3768faa7
Redwing <barrerasteve032@gmail.com>
2024-11-15 10:44:53 +0800

    Add project files.

---
+++ SENDSMS/SENDSMS/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        txtPhoneNumber = New TextBox()
+        txtMessage = New TextBox()
+        btnSendSMS = New Button()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(175, 71)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(108, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Phone Number"
+        ' 
+        ' txtPhoneNumber
+        ' 
+        txtPhoneNumber.Location = New Point(289, 64)
+        txtPhoneNumber.Name = "txtPhoneNumber"
+        txtPhoneNumber.Size = New Size(296, 27)
+        txtPhoneNumber.TabIndex = 1
+        ' 
+        ' txtMessage
+        ' 
+        txtMessage.Location = New Point(289, 124)
+        txtMessage.Multiline = True
+        txtMessage.Name = "txtMessage"
+        txtMessage.Size = New Size(296, 27)
+        txtMessage.TabIndex = 2
+        ' 
+        ' btnSendSMS
+        ' 
+        btnSendSMS.Location = New Point(342, 193)
+        btnSendSMS.Name = "btnSendSMS"
+        btnSendSMS.Size = New Size(94, 29)
+        btnSendSMS.TabIndex = 3
+        btnSendSMS.Text = "SEND SMS"
+        btnSendSMS.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(175, 127)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(104, 20)
+        Label2.TabIndex = 4
+        Label2.Text = "Message Mo : "
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label2)
+        Controls.Add(btnSendSMS)
+        Controls.Add(txtMessage)
+        Controls.Add(txtPhoneNumber)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtPhoneNumber As TextBox
+    Friend WithEvents txtMessage As TextBox
+    Friend WithEvents btnSendSMS As Button
+    Friend WithEvents Label2 As Label
+
+End Class
---
+++ SENDSMS/SENDSMS/Form1.vb
@@ -0,0 +1,70 @@
+﻿Imports System.IO.Ports
+Imports System.Threading
+
+Public Class Form1
+    Private WithEvents serialPort As New SerialPort()
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        serialPort.PortName = "COM3" ' Change this to your modem's COM port
+        serialPort.BaudRate = 9600
+        serialPort.Parity = Parity.None
+        serialPort.DataBits = 8
+        serialPort.StopBits = StopBits.One
+        serialPort.Handshake = Handshake.None
+        serialPort.RtsEnable = True
+        serialPort.DtrEnable = True
+
+        Try
+            serialPort.Open()
+            MessageBox.Show("Modem Connected")
+        Catch ex As Exception
+            MessageBox.Show("Could not connect to modem: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnSendSMS_Click(sender As Object, e As EventArgs) Handles btnSendSMS.Click
+        Dim phoneNumber As String = txtPhoneNumber.Text
+        Dim message As String = txtMessage.Text
+
+        ' Basic input validation
+        If String.IsNullOrEmpty(phoneNumber) Or String.IsNullOrEmpty(message) Then
+            MessageBox.Show("Please enter both a phone number and a message.")
+            Return
+        End If
+
+        ' Validate Philippine phone number format
+        If Not phoneNumber.StartsWith("+63") OrElse phoneNumber.Length <> 13 Then
+            MessageBox.Show("Please enter a valid Philippine phone number in international format (+63XXXXXXXXXXX).")
+            Return
+        End If
+
+        Try
+            ' Set the modem to text mode
+            serialPort.Write("AT+CMGF=1" & vbCrLf)
+            Thread.Sleep(1000)
+
+            ' Specify the phone number to send the message to
+            serialPort.Write("AT+CMGS=""" & phoneNumber & """" & vbCrLf)
+            Thread.Sleep(1000)
+
+            ' Write the message followed by Ctrl+Z (ASCII 26) to send
+            serialPort.Write(message & Chr(26))
+            MessageBox.Show("Message sent successfully.")
+
+        Catch ex As Exception
+            MessageBox.Show("Failed to send message: " & ex.Message)
+        End Try
+    End Sub
+
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        If serialPort.IsOpen Then
+            serialPort.Close()
+        End If
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+End Class
